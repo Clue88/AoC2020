64e306825735d2d1e823449af1fdfe7a5684fde3
Christopher Liu <christopherliu@sorcero.com>
2020-12-05 17:51:17 -0500

    Day 5: Binary Boarding

--- Day 5/5-1.py
+++ Day 5/5-1.py
@@ -1,13 +1,26 @@
 def main():
     f = open('5-input.txt', 'r')
-    lines = f.read().split('\n')[:-1]
+    seats = f.read().split('\n')[:-1]
     f.close()
 
-    # code
+    highest = 0
+    for seat in seats:
+        row_sum = 0
+        row = seat[0:7]
+        for ind, char in enumerate(row[::-1]):
+            if char == 'B': row_sum += 2 ** int(ind)
 
-    print('Result:')
+        col_sum = 0
+        col = seat[7:]
+        for ind, char in enumerate(col[::-1]):
+            if char == 'R': col_sum += 2 ** int(ind)
+
+        if int(row_sum) * 8 + int(col_sum) > highest:
+            highest = int(row_sum) * 8 + int(col_sum)
+
+    print('Result:', highest)
 
 if __name__ == '__main__':
     main()
 
-# Result: 
+# Result: 959
--- Day 5/5-2.py
+++ Day 5/5-2.py
@@ -1,13 +1,30 @@
 def main():
     f = open('5-input.txt', 'r')
-    lines = f.read().split('\n')[:-1]
+    seats = f.read().split('\n')[:-1]
     f.close()
 
-    # code
+    ids = []
+    for seat in seats:
+        row_sum = 0
+        row = seat[0:7]
+        for ind, char in enumerate(row[::-1]):
+            if char == 'B': row_sum += 2 ** int(ind)
 
-    print('Result:')
+        col_sum = 0
+        col = seat[7:]
+        for ind, char in enumerate(col[::-1]):
+            if char == 'R': col_sum += 2 ** int(ind)
+        
+        ids.append(int(row_sum) * 8 + int(col_sum))
+
+    for id in ids:
+        if id + 2 in ids and not id + 1 in ids:
+            result = id + 1
+            break
+
+    print('Result:', result)
 
 if __name__ == '__main__':
     main()
 
-# Result: 
+# Result: 527
